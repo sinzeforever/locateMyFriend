8bed21b85638e06f4859f586bedd988e5509c8c2
Sinze <sinze@yahoo-inc.com>
2015-03-12 13:09:26 +0800

    Add memberlist and adapter v1

--- app/src/main/java/hkec/yahoo/locatemyfriends/MemberListFragment.java
+++ app/src/main/java/hkec/yahoo/locatemyfriends/MemberListFragment.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import demo.android.jonaswu.yahoo.com.hackday_demo_lib.API;
 import demo.android.jonaswu.yahoo.com.hackday_demo_lib.BaseFragment;
 import demo.android.jonaswu.yahoo.com.hackday_demo_lib.LocationSyncroner;
 
 /**
- * Created by sinze on 3/10/15.
+ * Created by sinze on 3/12/15.
  */
-public class SettingFragment extends BaseFragment{
+public class MemberListFragment extends BaseFragment {
+
+    private View rootView;
+    private ListView memberList;
+    private MemberListAdapter memberListAdapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +26,15 @@ public class SettingFragment extends BaseFragment{
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.setting, container, false);
+        rootView = inflater.inflate(R.layout.my_group, container, false);
+        return rootView;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
     @Override
     public void onEventMainThread(API.ReturnDataEvent dma) {
 
